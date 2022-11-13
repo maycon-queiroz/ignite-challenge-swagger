c982e32fc096ff590a7177b76f371791243a5df7
Maycon Queiroz <fmaycon@gmail.com>
2022-11-13 13:17:39 -0300

    feat(swagger): add route post user

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,13 @@
 import express from "express";
+import swaggerUi from "swagger-ui-express";
 
+import swaggerFiler from "../swagger.json";
 import { usersRoutes } from "./routes/users.routes";
 
 const app = express();
 
 app.use(express.json());
+app.use("/api-doc", swaggerUi.serve, swaggerUi.setup(swaggerFiler));
 
 app.use("/users", usersRoutes);
 
